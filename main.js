954097b5c88fab1330464e3e6e8325cad7451d30
Danna Rubio <danna.1701625089@ucaldas.edu.co>
2021-09-08 16:21:57 -0500

    Capturar informacion

--- main.js
+++ main.js
@@ -4,6 +4,41 @@
 
 ((doc, win) => {
 	doc.addEventListener('DOMContentLoaded',event => {
-		// ...
+		obtenerValores();
+		
 	});
 })(document, window);
+
+function obtenerValores(){
+	const btnNumber = document.getElementsByName('numero');
+	
+		const btnOperacion = document.getElementsByName('operacion');
+		const btnIgual = document.getElementsByName('igual')[0];
+		const btnBorrar = document.getElementsByName('borrar')[0];
+		console.log(btnBorrar);
+		var result = document.getElementById('resultado');
+		btnNumber.forEach(function (boton) {
+			boton.addEventListener('click',function(){
+				//agregarNumero()
+				alert(boton.innerText);
+	})
+		});
+		btnOperacion.forEach(function (boton) {
+			boton.addEventListener('click',function(){
+				//SeleccionarOperacion()
+				alert(boton.innerText);
+	})
+		});
+		btnIgual.addEventListener('click',function(){
+			//caclcular();
+			//ActualizarDisplay();
+		});
+		btnBorrar.addEventListener('click',function(){
+			//borrar();
+			//ActualizarDsiplay();
+		});
+		
+}
+
+
+
