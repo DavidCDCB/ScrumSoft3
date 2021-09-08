63f99f42618e136ccd140ac7701a4db35d7225fe
DavidCDCB <cristian-cb1@hotmail.com>
2021-09-08 11:47:59 -0500

    Plantilla inicial

---
+++ main.js
@@ -0,0 +1,9 @@
+// https://desarrolloweb.com/manuales/manual-de-ecmascript-6.html
+
+'use strict';
+
+((doc, win) => {
+	doc.addEventListener('DOMContentLoaded',event => {
+		// ...
+	});
+})(document, window);
